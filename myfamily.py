f5c82aa94130b8a91cd97dc2aa2c4e0551b43908
angelaangelaa <demanenkoanzela640@gmail.com>
2025-02-02 16:30:00 +0600

    Update myfamily.py

--- myfamily.py
+++ myfamily.py
@@ -1,2 +1,18 @@
 myfamily = ("mother", "father", "sister", "brother", "sister") 
+
 print(myfamily)
+
+print(type(myfamily))  # Output: <class 'tuple'>
+
+print(myfamily[2])
+
+try:
+    myfamily.append("me")  #it's not working
+except AttributeError as e:
+    print("Error:", e)
+
+try:
+    myfamily.pop()  #also not working
+except AttributeError as e:
+    print("Error:", e)
+
